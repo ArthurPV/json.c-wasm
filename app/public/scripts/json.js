1490d7dc4f8815d42ccbbba2141d8d9bc84c753a
ArthurPV <arthurpontiervalois@gmail.com>
2025-08-11 00:58:05 -0400

    Fix a bug with non-ascii characters.

--- app/public/scripts/json.js
+++ app/public/scripts/json.js
@@ -1,4 +1,3 @@
-import { CString } from "./cstring.js"
 import * as Runtime from "./runtime.js";
 import * as CJson from "./c/json.js";
 import * as CAlloc from "./c/alloc.js";
@@ -9,14 +8,18 @@ Runtime.entrypoint(() => {
 		e.preventDefault();
 
 		const jsonEditorContent = Ace.jsonEditor.getValue();
-		const cJsonEditorContent = new CString(jsonEditorContent);
+		const textEncoder = new TextEncoder();
+		const jsonEditorContentBytes = textEncoder.encode(jsonEditorContent)
+		const jsonEditorContentBytesLength = jsonEditorContentBytes.length;
+		const cJsonEditorContent = CAlloc.malloc(4 * jsonEditorContentBytesLength);
 		const cErrorMessagePtr = CAlloc.malloc(4);
 		const cJsonSPtr = CAlloc.malloc(4);
 
+		Module.HEAP8.set(jsonEditorContentBytes, cJsonEditorContent);
 		Module.setValue(cErrorMessagePtr, 0, "*");
 		Module.setValue(cJsonSPtr, 0, "*");
 
-		CJson.parse(cJsonEditorContent.toPtr(), cJsonEditorContent.length, cErrorMessagePtr, cJsonSPtr);
+		CJson.parse(cJsonEditorContent, jsonEditorContentBytesLength, cErrorMessagePtr, cJsonSPtr);
 
 		const cErrorMessage = Module.getValue(cErrorMessagePtr, "*");
 
@@ -28,8 +31,8 @@ Runtime.entrypoint(() => {
 			Ace.resultEditor.setValue(Module.UTF8ToString(cJsonS));
 		}
 
+		CAlloc.free(jsonEditorContent);
 		CAlloc.free(cErrorMessagePtr);
 		CAlloc.free(cJsonSPtr);
-		cJsonEditorContent.free();
 	});
 });
